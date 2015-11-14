1b6bf6373a3de95ab10ddfa642f35e5dcb3b286b
AkashTyagi22 <AkashTyagi22@users.noreply.github.com>
2015-11-14 18:34:35 +0530

    Create work.php

---
+++ work.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang='en'>
+	
+
+<head>
+<style>
+p {
+    padding: 5px 15px; 
+    border: 1px solid black; 
+	background: blue;
+}  
+h2{
+    padding: 0px 5px 10px 3px; 
+    border: 1px solid red;
+	background: pink;
+} 
+</style>
+</head>
+		
+		<meta charset="utf-8" />
+		<title>HOME</title>
+		<link type="text/css" rel="stylesheet" media="screen" href="layout.css"/>
+        </head>
+		<body background="baby.jpg">
+			<h2><font color="white">DETAILS OF BABY NAMES</font></h2>
+			<table id="nav">
+				<tr>
+					<td></td>
+					<td><a href="work.php">HOME</a></td>
+					<td></td>
+                    <td><a href="tet.php">SEARCH</a></td>
+					<td></td>
+					<td><a href="graph.php">GRAPHICAL</a></td>
+					<td>
+					</td>
+		<td></td>
+		</tr>
+		</table>
+		<div>
+		
+						<form method="post" action="t.php">
+							enter the name for random search:<input type="text" name="search" required="required" />
+							<input type="submit" name="submit" value="search">
+							</form>
+		</div>
+		</body>
+		</html>
